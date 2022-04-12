571bd506b2e5a5086739d51cb429644aac502a22
Jaymin Patel <jaymin@weetechsolution.com>
2022-04-12 12:02:05 +0530

    changes in role entity

--- src/main/java/com/example/restassignment/entity/Role.java
+++ src/main/java/com/example/restassignment/entity/Role.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -16,6 +13,10 @@ import javax.persistence.Id;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private long roleId;
     private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
 }
