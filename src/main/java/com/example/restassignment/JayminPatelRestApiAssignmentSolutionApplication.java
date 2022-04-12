11e9a6aeeb5c2020ecb7f919bd507d8b69b72f96
Jaymin Patel <jaymin@weetechsolution.com>
2022-04-12 10:55:14 +0530

    Initial commit

---
+++ src/main/java/com/example/restassignment/JayminPatelRestApiAssignmentSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.restassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayminPatelRestApiAssignmentSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JayminPatelRestApiAssignmentSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restassignment/JayminPatelRestApiAssignmentSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JayminPatelRestApiAssignmentSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
